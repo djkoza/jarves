da93983562fd4ebf2f875ef749d535fd5effc8e5
Łukasz Kozikowski <lukasz.kozikowski@dotfusion.eu>
2017-03-31 17:39:00 +0200

    Image src - absolute path

--- ContentTypes/TypeImage.php
+++ ContentTypes/TypeImage.php
@@ -21,7 +21,7 @@ class TypeImage extends AbstractType
         if ($this->getContent()->getContent()) {
             $info = json_decode($this->getContent()->getContent(), true);
             if (isset($info['file'])) {
-                $path = substr(is_numeric($info['file']) ? $this->jarves->getWebFileSystem()->getPath($info['file']) : $info['file'], 1);
+                $path = substr(is_numeric($info['file']) ? $this->jarves->getWebFileSystem()->getPath($info['file']) : $info['file'], 0);
                 $width = $info['width'] ?: '100%';
                 $class = 'jarves-contentType-image align-' . (@$info['align'] ?: 'center');
                 return sprintf('<div class="%s"><img src="%s" width="%s"/></div>', $class, $path, $width);
